5c03e085f10dd0525744e7e4bfdf2fd777869f66
Taylor Wong <cytaylorw@gmail.com>
2020-09-24 16:26:53 +0800

    Updates

---
+++ precache-manifest.b88ce6b74007bad6513fee70c772fa54.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf97f874ce65310b9b59264e35714c3",
+    "url": "/react-codepen/index.html"
+  },
+  {
+    "revision": "4ab8cc9077f0fe6f68ba",
+    "url": "/react-codepen/static/css/2.ce280647.chunk.css"
+  },
+  {
+    "revision": "209714df50b543c3abbe",
+    "url": "/react-codepen/static/css/main.017a4f06.chunk.css"
+  },
+  {
+    "revision": "4ab8cc9077f0fe6f68ba",
+    "url": "/react-codepen/static/js/2.4bee8ef2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-codepen/static/js/2.4bee8ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "209714df50b543c3abbe",
+    "url": "/react-codepen/static/js/main.dfe858a4.chunk.js"
+  },
+  {
+    "revision": "1819c92a20dc8038edf3",
+    "url": "/react-codepen/static/js/runtime-main.97764405.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-codepen/static/media/logo.5d5d9eef.svg"
+  }
+]);
